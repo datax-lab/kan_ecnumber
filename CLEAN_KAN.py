a1900394da89a2fd814b516ba3cb81b928320f91
LouisD91 <59341452+LouisD91@users.noreply.github.com>
2024-10-02 15:23:51 -0700

    Update CLEAN_KAN.py

--- CLEAN_KAN.py
+++ CLEAN_KAN.py
@@ -5,11 +5,6 @@ from .efficient_kan import KAN
 class CLEAN_KAN(nn.Module):
     def __init__(self):
         super(LayerNormNet, self).__init__()
-        self.hidden_dim1 = hidden_dim
-        self.out_dim = out_dim
-        self.drop_out = drop_out
-        self.device = device
-        self.dtype = dtype
 
         self.fc1 = KAN([1280, 512], grid_size=3, spline_order=3)
         self.ln1 = nn.LayerNorm(512)
